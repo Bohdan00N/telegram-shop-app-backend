deada9b6a068c4493a050438133b10a6b48eb494
Bohdan Nalyvkin <nalivkin.bog@gmail.com>
2024-08-21 11:27:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require("node-telegram-bot-api");
 require("dotenv").config();
 const token = process.env.BOT_TOKEN;
-const webAppUrl = "https://mythaisushi.netlify.app/";
+const webAppUrl = process.env.WEB;
 const bot = new TelegramBot(token, { polling: true });
 const express = require("express");
 const cors = require("cors");
